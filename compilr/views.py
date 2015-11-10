4b5c3610fa7a5e0fc139f6cb8d7eecba3fbd9a0e
turing_machine <shivam@skillwiz.com>
2015-11-11 05:11:48 +0530

    printing output of api call

--- compilr/views.py
+++ compilr/views.py
@@ -12,12 +12,13 @@ def index(request):
 		#iinput = request.POST['iinput']
 		RUN_URL = u'https://api.hackerearth.com/v3/code/run/'
 		CLIENT_SECRET = '496c2cb7d30d44718e6ecf2b096d02f62112667f'
-		source = "print 'Hello World'"
+		source = "print int(raw_input())"
 
 		data = {
     	'client_secret': CLIENT_SECRET,
     	'async': 0,
     	'source': source,
+    	'input': 8,
     	'lang': "PYTHON",
     	'time_limit': 5,
     	'memory_limit': 262144,
@@ -26,7 +27,6 @@ def index(request):
 		r = requests.post(RUN_URL, data=data)
 		#print r.json()
 	 	#return HttpResponse( r.json() )
-		print 'Raw Data: "%s"' % request.body   
 		return HttpResponse(r)
 		
 	return render(request, 'compilr/index.html')
