5496b0a7aed0078a2826565d2b9215e9986993b7
turing_machine <shivam@skillwiz.com>
2015-11-11 21:12:23 +0530

    printing output of api call

--- compilr/views.py
+++ compilr/views.py
@@ -8,19 +8,20 @@ import requests
 def index(request):
 
 	if request.method == 'POST':
-		#lang = request.POST['lang']
-		#source = request.POST['source']
-		#iinput = request.POST['iinput']
+		lang = request.POST['lang']
+		source = request.POST['source']
+		iinput = request.POST['iinput']
+		
 		RUN_URL = u'https://api.hackerearth.com/v3/code/run/'
 		CLIENT_SECRET = '496c2cb7d30d44718e6ecf2b096d02f62112667f'
-		source = "print int(raw_input())"
+		#source = "print int(raw_input())"
 
 		data = {
     	'client_secret': CLIENT_SECRET,
     	'async': 0,
     	'source': source,
-    	'input': 8,
-    	'lang': "PYTHON",
+    	'input': iinput,
+    	'lang': lang,
     	'time_limit': 5,
     	'memory_limit': 262144,
 		}
